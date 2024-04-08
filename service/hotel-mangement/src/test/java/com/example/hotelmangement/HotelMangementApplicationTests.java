9a0031005ec7c5d157f32dea4686c95e481e6e10
Nikhil-Wadatkar-Basket <wadatkarnikhil22@gmail.com>
2024-04-08 08:40:01 +0530

    project strucutre added

---
+++ service/hotel-mangement/src/test/java/com/example/hotelmangement/HotelMangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotelmangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelMangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
